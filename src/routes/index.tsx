c32aed589e3f98120f1cf7fea65eed1d7f7e5e3d
Neuville <otiendeneville@gmail.com>
2026-08-12 18:57:08 +0300

    add checkboc

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,7 +8,7 @@ function Home() {
       <h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
       <p className="mt-4 text-lg">
         Edit <code>src/routes/index.tsx</code> to get started.
-      </p>
+      </p>   
     </div>
   )
 }
