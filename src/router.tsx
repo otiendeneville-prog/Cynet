8263ec1a4335c128fb3795eaa0f54bf5a11a7050
Neuville <otiendeneville@gmail.com>
2026-08-13 11:35:03 +0300

    add router

--- src/router.tsx
+++ src/router.tsx
@@ -1,19 +1,16 @@
-import { createRouter as createTanStackRouter } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen'
+import { QueryClient } from "@tanstack/react-query";
+import { createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
 
-export function getRouter() {
-  const router = createTanStackRouter({
+export const getRouter = () => {
+  const queryClient = new QueryClient();
+
+  const router = createRouter({
     routeTree,
+    context: { queryClient },
     scrollRestoration: true,
-    defaultPreload: 'intent',
     defaultPreloadStaleTime: 0,
-  })
+  });
 
-  return router
-}
-
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: ReturnType<typeof getRouter>
-  }
-}
+  return router;
+};
