2f69863571cfa7a2b125bb6295e962ca1b82f311
Neuville <otiendeneville@gmail.com>
2026-09-16 11:33:00 +0300

    rendering lable

--- src/components/ui/dropdown-menu.tsx
+++ src/components/ui/dropdown-menu.tsx
@@ -10,14 +10,12 @@ type MenuLink= {
 
 function MegaMenu({ label,links}:{label:string:links:MenuLink[]}){
 <div className="flex flex-col">
+    <label className="font-bold mb-5">{label}</label>
      <Link 
        key={label.links}
        to={Link.to}
        hash={hash.link}
-     
      >
-         
-     
      </Link>
 </div>
 }
