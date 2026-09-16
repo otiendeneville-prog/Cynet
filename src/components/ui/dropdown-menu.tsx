352b3fbe53ee2905bda76bc6220c85e7dd742134
Neuville <otiendeneville@gmail.com>
2026-09-16 11:08:49 +0300

    add menu

--- src/components/ui/dropdown-menu.tsx
+++ src/components/ui/dropdown-menu.tsx
@@ -1,3 +1,12 @@
-export default function dropdown() {
-  return <div>dropdown-menu</div>
+import { Link } from "lucide-react";
+
+type MenuLink= {
+   label:string;
+   to:string;
+   harsh ?:string;
 }
+
+function MegaMenu({lable,links}:{label:string:links:MenuLink[]}){
+
+}
+
