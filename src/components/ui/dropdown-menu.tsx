57ca473de1a5fa577c1ff9b02ac15fb25b9ae55b
Neuville <otiendeneville@gmail.com>
2026-09-16 12:17:46 +0300

    addding basic props

--- src/components/ui/dropdown-menu.tsx
+++ src/components/ui/dropdown-menu.tsx
@@ -1,20 +1,18 @@
+import { hash } from 'crypto'
 import { Link } from 'lucide-react'
 import { Label } from 'radix-ui'
-type MenuLink = {
+
+type Props = {
   label: string
   to: string
   hash?: string
 }
 
-export function MegaMenu({
-  label,
-  links,
-}: {
-  label: string
-  links: MenuLink[]
-}) {
-  ;<div className="flex flex-col">
-    <label className="font-bold mb-5">{label}</label>
-    <Link key={label.Link} to={links.to} hash={hash.link}></Link>
-  </div>
+export function MegaMenu({ label }: Props) {
+  return (
+    <div className="flex flex-col">
+      <label className="font-bold mb-5">{label}</label>
+      <Link key={label}></Link>
+    </div>
+  )
 }
