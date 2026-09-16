f21a05ff7ddb343260135120be25af6e0e7bafc2
Neuville <otiendeneville@gmail.com>
2026-09-16 11:51:45 +0300

    export megamenu

--- src/components/ui/dropdown-menu.tsx
+++ src/components/ui/dropdown-menu.tsx
@@ -1,22 +1,20 @@
-import { link } from "fs";
-import { Link } from "lucide-react";
-import { Label } from "radix-ui";
+import { Link } from 'lucide-react'
 
-type MenuLink= {
-   label:string;
-   to:string;
-   hash ?:string;
+type MenuLink = {
+  label: string
+  to: string
+  hash?: string
 }
 
-function MegaMenu({ label,links}:{label:string:links:MenuLink[]}){
-<div className="flex flex-col">
+export function MegaMenu({
+  label,
+  links,
+}: {
+  label: string
+  links: MenuLink[]
+}) {
+  ;<div className="flex flex-col">
     <label className="font-bold mb-5">{label}</label>
-     <Link 
-       key={label.links}
-       to={Link.to}
-       hash={hash.link}
-     >
-     </Link>
-</div>
+    <Link key={label.Link} to={links.to} hash={hash.link}></Link>
+  </div>
 }
-
